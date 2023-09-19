7bd411925874128a00c318cede158ae1eda6d873
Adrian Martinez <afmartinez1215@gmail.com>
2023-09-18 22:01:04 -0400

    Adding unit tests for __main__.py

---
+++ tests/test_main.py
@@ -0,0 +1,46 @@
+import pytest
+import tempfile
+import yaml
+from pathlib import Path
+from click.testing import CliRunner
+from yaml2dot.__main__ import render_yaml
+
+@pytest.fixture
+def temp_dir():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        yield Path(temp_dir)
+
+def test_render_yaml(temp_dir, capfd):
+    yaml_data = [
+        {'Key': 'key1', 'Value': 'value1'},
+        {'Key': 'key2', 'Value': {'nested_key': 'nested_value'}},
+        {'Key': 'key3', 'Value': [1, 2, 3]},
+    ]
+
+    yaml_file = temp_dir / "test.yaml"
+    dot_file = temp_dir / "test.dot"
+
+    with open(yaml_file, "w") as f:
+        yaml.dump(yaml_data, f)
+
+    runner = CliRunner()
+    result = runner.invoke(render_yaml, [f"--input-file={yaml_file}", f"--output-file={dot_file}", "--rankdir=LR"])
+
+    assert dot_file.exists()
+
+    # Capture the printed output and assert on it
+    captured = capfd.readouterr()
+    assert "Error" not in captured.err  # Make sure there are no error messages
+
+def test_render_yaml_invalid_input(temp_dir):
+    # Create an invalid YAML file
+    yaml_file = temp_dir / "invalid.yaml"
+    with open(yaml_file, "w") as f:
+        f.write("key1: value1\nkey2: value2\nkey3: value3:")
+
+    runner = CliRunner()
+    result = runner.invoke(render_yaml, [f"--input-file={yaml_file}", "--output-file=tst.dot", "--rankdir=LR"])
+
+    # Check if the result contains the expected error message
+    assert "Error" in result.output
+    assert not (temp_dir / "test.dot").exists()
