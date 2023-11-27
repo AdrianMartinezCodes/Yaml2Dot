0799d83429353f1bd8ca1dd17db73fe714d184f7
Adrian Martinez <afmartinez1215@gmail.com>
2023-11-26 22:53:36 -0500

    Changing default arg

--- yaml2dot/__main__.py
+++ yaml2dot/__main__.py
@@ -15,7 +15,7 @@ from yaml2dot.renderer import render
 @click.option("--rankdir", type=click.Choice(['LR', 'TB']), default='LR', help="Rank direction (LR for left to right, TB for top to bottom).")
 @click.option("--output-format", type=click.Choice(['dot', 'json', '']), default='dot', help="Output format (DOT or JSON).")
 @click.option("--multi-view", is_flag=True, help="Enable alternative graph view for multiple YAML documents.")
-@click.option("--round-robin", is_flag=True, help="Enable Round Robin Node Style. If not, defaults to user defined shapes.")
+@click.option("--round-robin", is_flag=True, help="Enable Round Robin Node Style. If not, defaults to 'rounded' shape.")
 @click.option("--shape", type=click.STRING, default="rounded", help="User defined node shape. Default='rounded'. See graphviz page: https://graphviz.org/doc/info/shapes.html for support shapes.")
 def render_yaml(input_file, output_file, rankdir, output_format, multi_view, round_robin, shape):
     """
