b4d0c655595cc2e5d1b912bece39229281267c2f
Adrian Martinez <afmartinez1215@gmail.com>
2023-11-26 23:47:41 -0500

    Updating library api for new customization

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -31,13 +31,16 @@ sample_json_data = {
     }]
 }
 
-
 def test_convert_yaml_to_dot():
     dot_output = convert_yaml_or_json_to_format(sample_yaml_data,
-                                                output_format='dot')
+                                                output_format='dot',
+                                                multi_view=False,
+                                                round_robin=False,
+                                                shape='rounded')
     assert dot_output is not None
     assert isinstance(dot_output, str)
-    # You can add further assertions based on the expected DOT output.
+    assert "shape=rounded" in dot_output
+
 
 
 def test_convert_yaml_to_json():
@@ -45,7 +48,7 @@ def test_convert_yaml_to_json():
                                                  output_format='json')
     assert json_output is not None
     assert isinstance(json_output, str)
-    # You can add further assertions based on the expected JSON output.
+    assert "nodes" in json_output
 
 
 def test_convert_json_to_dot():
@@ -53,7 +56,6 @@ def test_convert_json_to_dot():
                                                 output_format='dot')
     assert dot_output is not None
     assert isinstance(dot_output, str)
-    # You can add further assertions based on the expected DOT output.
 
 
 def test_convert_json_to_json():
@@ -61,7 +63,7 @@ def test_convert_json_to_json():
                                                  output_format='json')
     assert json_output is not None
     assert isinstance(json_output, str)
-    # You can add further assertions based on the expected JSON output.
+    assert "nodes" in json_output
 
 
 def test_invalid_output_format():
@@ -78,3 +80,30 @@ def test_invalid_data():
                                                  output_format='json')
     assert dot_output is None
     assert json_output is None
+    
+def test_convert_with_custom_node_attrs():
+    custom_attrs = {"color": "red", "style": "filled"}
+    dot_output = convert_yaml_or_json_to_format(sample_yaml_data, output_format='dot', user_node_attrs=custom_attrs)
+    assert dot_output is not None
+    assert "color=red" in dot_output
+    assert "style=filled" in dot_output
+
+def test_convert_with_multi_view():
+    dot_output = convert_yaml_or_json_to_format(sample_yaml_data,
+                                                output_format='dot',
+                                                multi_view=True)
+    assert dot_output is not None
+
+def test_convert_with_round_robin():
+    dot_output = convert_yaml_or_json_to_format(sample_yaml_data,
+                                                output_format='dot',
+                                                round_robin=True)
+    assert dot_output is not None
+
+def test_convert_with_specific_shape():
+    shape = "box"
+    dot_output = convert_yaml_or_json_to_format(sample_yaml_data,
+                                                output_format='dot',
+                                                shape=shape)
+    assert dot_output is not None
+    assert "shape=box" in dot_output
--- yaml2dot/converter.py
+++ yaml2dot/converter.py
@@ -7,28 +7,34 @@ from networkx.readwrite import json_graph
 from yaml2dot.renderer import render
 
 
-def convert_yaml_or_json_to_format(data: Union[dict, None],
-                                   output_format: str = 'dot',
-                                   rankdir: str = 'LR') -> Optional[str]:
+def convert_yaml_or_json_to_format(
+    data: Union[dict, None],
+    user_node_attrs: dict = None,
+    output_format: str = 'dot',
+    rankdir: str = 'LR',
+    multi_view: bool = False,
+    round_robin: bool = False,
+    shape: str = 'rounded'
+) -> Optional[str]:
     """
     Convert YAML or JSON data to DOT or JSON format.
 
     Parameters:
     - data (Union[dict, None]): The input YAML or JSON data as a dictionary or None if there was an error.
+    - user_node_attrs (Dict[str, Any], optional): User-defined attributes for each node.
     - output_format (str): Output format ('dot' or 'json'). Default is 'dot'.
     - rankdir (str): Rank direction for the layout (LR for left to right, TB for top to bottom). Default is 'LR'.
+    - multi_view (bool): Enable alternative graph view for multiple YAML documents. Default is False.
+    - round_robin (bool): Enable Round Robin Node Style. If not, defaults to user-defined shapes. Default is False.
+    - shape (str): User-defined node shape. Default is 'rounded'.
 
     Returns:
     - Optional[str]: The converted data in DOT or JSON format as a string or None if there was an error.
     """
-    if data is None:
+    if data is None or not isinstance(data, dict):
         return None
 
-    if not isinstance(data, dict):
-        return None
-    # We can probably include more data validations
-
-    nx_graph = render(data, rankdir=rankdir)
+    nx_graph = render(data,user_node_attrs=user_node_attrs, rankdir=rankdir, multi_view=multi_view, round_robin=round_robin, shape=shape)
 
     if output_format == 'dot':
         # Convert the graph to DOT format
